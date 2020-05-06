514c513378a0ead612d567eaa4fee317b6a249a8
Shaunz0r <shaunz0r@gmail.com>
2020-05-06 15:57:25 +0100

    trying my best lol

--- masterMind.js
+++ masterMind.js
@@ -6,35 +6,26 @@ window.onload = function () {
 
     for (let index = 0; index < guessPiece.length; index++) {
         const element = guessPiece[index];
-        element.addEventListener("click", colorPick);
+        element.addEventListener("click", colourToggle);
     }
-
 }
 
-function colorPick(event)  {
-    // event is automatically passed in to event listeners
-    // it contains details on what happened
-    // we need to get the current element out of this event, which is conveniently
-    // done via the .target property
-
-    console.log("piece clicked!"); // just to show that the event fires when a piece is clicked
-
-    var currentElement = event.target; // gets the current html element from the event (i.e. the one that was clicked)
-    var currentElementColour = currentElement.style.backgroundColor; // as a note here, this will be an empty string - do you know why?
-    var colourToSet;
-
-    if (currentElementColour === '#000326') {
-        colourToSet = '#f00';
-    } else if (currentElementColour === '#f00') {
-        colourToSet = '#0f0';
-    } else if (currentElementColour === '0f0') {
-        colourToSet = '#00f';
+var colours = ["red", "green", "blue", "yellow" ,"purple", "white"];
+function colourToggle(event) {
+    var element = event.target;
+    
+    var currentColour = element.classList[1]; // [0] will be piece, [1] will be colour
+    
+    var index = colours.indexOf(currentColour);
+    index++; // next element in the array
+    
+    
+    if(index > 5) {
+      index = 0;
     }
+    
+    element.classList.remove(currentColour);
+    element.classList.add(colours[index]);
+    
+  }
 
-    currentElement.style.backgroundColor = colourToSet; // ultimately, an undefined value will be assigned to the piece background colour
-
-    // TO DO:
-    // Think about where currentElement.style.backgroundColor looks for it's information compared to where you have it
-        // Check this page out, it'll help with the above: https://www.w3schools.com/jsref/prop_html_style.asp
-
-};
