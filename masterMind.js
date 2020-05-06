eaf7c59a3026bfe8f71c7357ed8dd1efffc9423a
Shaunz0r <shaunz0r@gmail.com>
2020-05-06 11:25:29 +0100

    functionChange

--- masterMind.js
+++ masterMind.js
@@ -1,14 +1,15 @@
+window.onload = function (){
 const guessPiece = document.getElementsByClassName('piece');
-const targetPiece = document.getElementsByClassName('targetPiece')
-let guessPieceColor = guessPiece.style.backgroundColor;
- let colorPick = () => {
-    if (guessPieceColor === '#000326') {
-        guessPieceColor = '#f00';
-    } else if (guessPieceColor === '#f00') {
-        guessPieceColor = '#0f0';
-    } else if (guessPieceColor === '0f0') {
-        guessPieceColor = '#00f';
+guessPiece.onclick = function colorPick()  {
+    if (this.style.backgroundColor === '#000326') {
+        this.style.backgroundColor = '#f00';
+    } else if (this.style.backgroundColor === '#f00') {
+        this.style.backgroundColor = '#0f0';
+    } else if (this.style.backgroundColor === '0f0') {
+        this.style.backgroundColor = '#00f';
     }
-    piece.style.backgroundColor = guessPieceColor;
+    piece.style.backgroundColor = this.style.backgroundColor;
 };
-guessPiece.onclick = colorPick();
+
+
+}
