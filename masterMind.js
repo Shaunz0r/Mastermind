66e8d15d1f3f1ce15669241ecffc05de8d60522b
Shaunz0r <shaunz0r@gmail.com>
2020-05-06 10:34:26 +0100

    JSfirstattempt

--- masterMind.js
+++ masterMind.js
@@ -1,3 +1,14 @@
-const pieces = {
-    
-}
+const guessPiece = document.getElementsByClassName('piece');
+const targetPiece = document.getElementsByClassName('targetPiece')
+let guessPieceColor = guessPiece.style.backgroundColor;
+ let colorPick = () => {
+    if (guessPieceColor === '#000326') {
+        guessPieceColor = '#f00';
+    } else if (guessPieceColor === '#f00') {
+        guessPieceColor = '#0f0';
+    } else if (guessPieceColor === '0f0') {
+        guessPieceColor = '#00f';
+    }
+    piece.style.backgroundColor = guessPieceColor;
+};
+guessPiece.onclick = colorPick();
